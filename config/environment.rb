7abd6aff0c0783bb8c88cb5b9a24b14b8fd83a8e
adi-botak- <adityautamawijaya@gmail.com>
2016-11-11 19:21:43 +0800

    add environment.rb

---
+++ config/environment.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'sequel'
+
+configure :development do
+  ENV['DATABASE_URL'] = 'sqlite://db/dev.db'
+end
+
+configure :test do
+  ENV['DATABASE_URL'] = 'sqlite://db/test.db'
+end
+
+configure :development, :production do
+  require 'hirb'
+  Hirb.enable
+end
+
+configure do
+  DB = Sequel.connect(ENV['DATABASE_URL'])
+end
