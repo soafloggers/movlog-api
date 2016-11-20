9c50fdbc38d3f8a5701482247c6a98d6bc6991bb
Yu-Shiang <z58085111@gmail.com>
2016-11-20 20:47:42 +0800

    remove representers/locations

--- representers/locations.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Represents overall group information for JSON API output
-class LocationsRepresenter < Roar::Decorator
-  include Roar::JSON
-
-  collection :locations, extend: LocationRepresenter, class: Location
-end
