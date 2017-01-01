4427abcf058badd8bebf7ded279983dca67887fb
Yu-Shiang <z58085111@gmail.com>
2017-01-01 17:48:37 +0800

    fix airport require

--- representers/airports_search_results.rb
+++ representers/airports_search_results.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require_relative 'room'
+require_relative 'airport'
 
 # Represents overall room information for JSON API output
 class AirportsSearchResultsRepresenter < Roar::Decorator
